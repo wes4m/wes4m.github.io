356a4ab78c697533f524c50900d65ff14101198f
Wesam Alzahir <wes4m@Wesams-MacBook-Pro.local>
2020-05-30 17:18:41 -0700

    Deployed

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+(() => {
+  // Theme switch
+  const body = document.body;
+  const lamp = document.getElementById("mode");
+  const data = body.getAttribute("data-theme");
+
+  const initTheme = (state) => {
+    if (state === "dark") {
+      body.setAttribute("data-theme", "dark");
+    } else if (state === "light") {
+      body.removeAttribute("data-theme");
+    } else {
+      localStorage.setItem("theme", data);
+    }
+  };
+
+  const toggleTheme = (state) => {
+    if (state === "dark") {
+      localStorage.setItem("theme", "light");
+      body.removeAttribute("data-theme");
+    } else if (state === "light") {
+      localStorage.setItem("theme", "dark");
+      body.setAttribute("data-theme", "dark");
+    } else {
+      initTheme(state);
+    }
+  };
+
+  initTheme(localStorage.getItem("theme"));
+
+  lamp.addEventListener("click", () =>
+    toggleTheme(localStorage.getItem("theme"))
+  );
+
+  // Blur the content when the menu is open
+  const cbox = document.getElementById("menu-trigger");
+
+  cbox.addEventListener("change", function () {
+    const area = document.querySelector(".wrapper");
+    this.checked
+      ? area.classList.add("blurry")
+      : area.classList.remove("blurry");
+  });
+})();
